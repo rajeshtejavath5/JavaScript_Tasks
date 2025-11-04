8a662d06e318e967b553782edd0c5ed8ff8fc0e9
Rajesh <rajeshtejavath4422@gmail.com>
2025-11-04 23:55:14 +0530

    bulb task

---
+++ Tasks/Bulb_Switch/main.js
@@ -0,0 +1,6 @@
+function bulb_on(){
+    document.getElementById("offimg").src="https://png.pngtree.com/png-vector/20250612/ourmid/pngtree-glowing-yellow-light-bulb-isolated-on-transparent-background-png-image_16524952.png"
+}
+function bulb_off(){
+    document.getElementById("offimg").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwr9s3piYLrGtqsBg_97Q7eweJRx6e20Gts2zEwhllbbuMY5gNbmjXjBxoDK9jD3iAtP8&usqp=CAU"
+}    
