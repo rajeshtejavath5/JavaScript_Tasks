158510e5c8ed8bdd08dd9119ed67b2bc0f0dad24
Rajesh <rajeshtejavath4422@gmail.com>
2025-11-05 21:45:55 +0530

    feature dynamic color change name is added

--- Tasks/Bg_change_vth_buttons/js_script.js
+++ Tasks/Bg_change_vth_buttons/js_script.js
@@ -1,15 +1,21 @@
 function redbg() {
             document.body.style.backgroundColor = "red"
+            r=document.getElementById("h1").innerHTML="Current Color:RED"
+            console.log(r)
         }
         function bluebag() {
             document.body.style.backgroundColor = "blue"
+             r=document.getElementById("h1").innerHTML="Current Color:Blue"
         }
         function orangebg() {
             document.body.style.backgroundColor = "rgb(230, 97, 3)"
+             r=document.getElementById("h1").innerHTML="Current Color:Orange"
         }
         function blackbg() {
             document.body.style.backgroundColor = "black"
+             r=document.getElementById("h1").innerHTML="Current Color:Black"
         }
         function whitebg() {
             document.body.style.backgroundColor = "white"
+             r=document.getElementById("h1").innerHTML="You can click on buttons to change background color of body"
         }
