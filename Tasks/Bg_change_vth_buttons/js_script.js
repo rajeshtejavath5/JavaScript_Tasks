a56ff411d845e77e109e4d555759322f2befe520
Rajesh <rajeshtejavath4422@gmail.com>
2025-11-05 17:21:31 +0530

    basic dom

---
+++ Tasks/Bg_change_vth_buttons/js_script.js
@@ -0,0 +1,15 @@
+function redbg() {
+            document.body.style.backgroundColor = "red"
+        }
+        function bluebag() {
+            document.body.style.backgroundColor = "blue"
+        }
+        function orangebg() {
+            document.body.style.backgroundColor = "rgb(230, 97, 3)"
+        }
+        function blackbg() {
+            document.body.style.backgroundColor = "black"
+        }
+        function whitebg() {
+            document.body.style.backgroundColor = "white"
+        }
